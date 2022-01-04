638b24f6a9b796f53dded1277e7f76dc0d407b49
ymd65536 <ymd65536@gmail.com>
2022-01-04 13:06:02 +0900

    RegisterClipText_Click 追加

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,11 +15,21 @@ using System.Windows.Shapes;
 
 namespace ToClip
 {
+
   public partial class MainWindow : Window
   {
     public MainWindow()
     {
       InitializeComponent();
     }
+    private void RegisterClipText_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+      if (!(this.SaveTextBox.Text == ""))
+      {
+        this.ClipList.Items.Add(SaveTextBox.Text.ToString());
+        this.SaveTextBox.Text = "";
+      }
+    }
+
   }
 }
